e2f6142804910343aeeb6b43f0dc901035e1ab5e
Actsys 10 <Actsys 10@DESKTOP-Q76TTOQ>
2025-03-29 11:31:47 +0530

    push to main branch

---
+++ src/test/java/application/application/com/ApplicationTests.java
@@ -0,0 +1,13 @@
+package application.application.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
